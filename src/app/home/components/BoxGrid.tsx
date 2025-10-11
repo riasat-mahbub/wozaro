cc67b5ca58c0b8a49a5885ce49a268a1fdeb3589
riasat1998 <riasat1998@gmail.com>
2025-10-11 20:22:05 -0300

    made boxgrid centered

--- src/app/home/components/BoxGrid.tsx
+++ src/app/home/components/BoxGrid.tsx
@@ -4,10 +4,13 @@ import { Box } from "./Box"
 export const BoxGrid = () =>{
     const boxes = Array.from(Array(25).keys())
     return(
-        <div className="grid grid-cols-5 justify-center items-center w-[31rem] h-[31rem]">
-            {boxes.map( (i) =>(
-                <Box key={i}/>
-            ))}
+        <div className="w-full flex justify-center">
+            <div className="grid grid-cols-5 justify-center items-center w-[31rem] h-[31rem] mt-10">
+                {boxes.map( (i) =>(
+                    <Box key={i}/>
+                ))}
+            </div>
         </div>
+
     )
 }
